9b14a6b0616204489a3b3bf82cf9defcd6287017
bryanblanco10 <ing.bryanblanco10@gmail.com>
2023-08-27 01:43:45 -0500

    prueba de deploy

--- src/config/api/index.ts
+++ src/config/api/index.ts
@@ -1,8 +1,9 @@
 const API_VERSION = import.meta.env.VITE_API_VERSION;
 const API_URL = import.meta.env.VITE_API_URL;
+console.log(API_URL);
 export const API_ROUTES = {
-  country: {
-    get: `${API_URL}/${API_VERSION}/all`,
-    find: `${API_URL}/${API_VERSION}/alpha`,
-  },
+	country: {
+		get: `${API_URL}/${API_VERSION}/all`,
+		find: `${API_URL}/${API_VERSION}/alpha`,
+	},
 };
